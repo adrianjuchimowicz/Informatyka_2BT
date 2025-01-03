feba2aac05e531b250d783694a08d24615dd8edc
Adrian Juchimowicz <adrian.juchimowicz@infotech.edu.pl>
2025-01-03 11:32:40 +0100

    ..

---
+++ klkl.py
@@ -0,0 +1,17 @@
+def horner(liczba,x):
+    w = int(liczba[0])
+    for i in range(1,len(liczba)):
+        w = x * w + int(liczba[i])
+    return w
+
+def decToAll(liczba,system):
+    wynik = ""
+    while liczba > 0:
+        wynik = str(liczba%system) + wynik
+        liczba = liczba // system
+    return wynik
+
+print(decToAll(449,8))
+print(horner("11010",2))
+print(horner("701",8))
+print(horner("302",4))
